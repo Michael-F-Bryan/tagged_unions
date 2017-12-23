a84733d15c9fc89fa0525f653cbc7db4001a9468
Michael Bryan <michaelfbryan@gmail.com>
2017-12-23 20:27:27 +0800

    Commented out the placeholder code

--- tagged_union_derive/src/codegen.rs
+++ tagged_union_derive/src/codegen.rs
@@ -51,34 +51,36 @@ fn generate_tagged_union_impl(base_name: &str, tags: &[Tag], typemap: &TypeMap) 
 
     let as_tagged_method = quote!{
         fn as_tagged(&self) -> Self::Target {
-            match *self {
-                Message::Halt => TaggedMessage {
-                    tag: MESSAGE_HALT,
-                    kind: MessageKind { empty: () },
-                },
-                Message::Wait(n) => TaggedMessage {
-                    tag: MESSAGE_WAIT,
-                    kind: MessageKind { wait: n },
-                },
-                Message::Move(n) => TaggedMessage {
-                    tag: MESSAGE_MOVE,
-                    kind: MessageKind { move_: n },
-                },
-            }
+            unimplemented!()
+            // match *self {
+            //     Message::Halt => TaggedMessage {
+            //         tag: MESSAGE_HALT,
+            //         kind: MessageKind { empty: () },
+            //     },
+            //     Message::Wait(n) => TaggedMessage {
+            //         tag: MESSAGE_WAIT,
+            //         kind: MessageKind { wait: n },
+            //     },
+            //     Message::Move(n) => TaggedMessage {
+            //         tag: MESSAGE_MOVE,
+            //         kind: MessageKind { move_: n },
+            //     },
+            // }
         }
     };
 
     let from_tagged_method = quote!{
         unsafe fn from_tagged(tagged: &Self::Target) -> Result<Self, InvalidTag> {
-            match tagged.tag {
-                MESSAGE_HALT => Ok(Message::Halt),
-                MESSAGE_WAIT => Ok(Message::Wait(tagged.kind.wait)),
-                MESSAGE_MOVE => Ok(Message::Move(tagged.kind.move_)),
-                _ => Err(InvalidTag {
-                    got: tagged.tag,
-                    possible_tags: 0..3,
-                }),
-            }
+            unimplemented!()
+            // match tagged.tag {
+            //     MESSAGE_HALT => Ok(Message::Halt),
+            //     MESSAGE_WAIT => Ok(Message::Wait(tagged.kind.wait)),
+            //     MESSAGE_MOVE => Ok(Message::Move(tagged.kind.move_)),
+            //     _ => Err(InvalidTag {
+            //         got: tagged.tag,
+            //         possible_tags: 0..3,
+            //     }),
+            // }
         }
     };
 
